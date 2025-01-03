6ca2ada19f27d3d8811337cfe57eb9330c6829e0
Juan <juanfajardoreyes@hotmail.com>
2025-01-03 11:45:30 +0100

    Archivos de configuración esenciales

---
+++ src/main/java/com/reikitubienestar/reiki_rest/ReikiRestApplication.java
@@ -0,0 +1,13 @@
+package com.reikitubienestar.reiki_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReikiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReikiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reikitubienestar/reiki_rest/ReikiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reikitubienestar.reiki_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReikiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
